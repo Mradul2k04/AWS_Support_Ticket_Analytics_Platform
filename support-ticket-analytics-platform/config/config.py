9095e885945263413a9594a076342d2ff24c40ba
KaranNegi08 <karannegi.agra@gmail.com>
2026-06-26 14:51:15 +0530

    added env variables in config

--- support-ticket-analytics-platform/config/config.py
+++ support-ticket-analytics-platform/config/config.py
@@ -1,5 +1,13 @@
-AWS_REGION= "eu-north-1"
-BUCKET_NAME = "ticket-analytics-platform"
+import os
+from dotenv import load_dotenv
 
-RAW_PREFIX = "raw/"
-CURATED_PREFIX = "curated/"
+load_dotenv()
+
+AWS_REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+RAW_PREFIX = os.getenv("RAW_PREFIX")
+CURATED_PREFIX = os.getenv("CURATED_PREFIX")
+
+ISSUES_FILE = os.getenv("ISSUES_FILE")
+HISTORY_FILE = os.getenv("HISTORY_FILE")
