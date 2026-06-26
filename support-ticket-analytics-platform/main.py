55dad8e06da86a542b7bea0120005f1d4f1d8710
KaranNegi08 <karannegi.agra@gmail.com>
2026-06-26 14:51:47 +0530

    calling extract method

--- support-ticket-analytics-platform/main.py
+++ support-ticket-analytics-platform/main.py
@@ -2,6 +2,8 @@ from aws.s3_bucket.create_bucket import create_bucket
 from aws.s3_bucket.create_folder import create_folder
 from aws.s3_bucket.upload_file import upload_file
 from config.config import RAW_PREFIX
+from pyspark_folder.extraction.extract import extract,create_spark_session
+from pyspark.sql import SparkSession
 
 def main():
     
@@ -15,5 +17,13 @@ def main():
     # upload_file("datasets/issues.csv", RAW_PREFIX + "issues.csv")
     # upload_file("datasets/issues_change_history.csv",RAW_PREFIX + "issues_change_history.csv")
 
+    spark = create_spark_session()
+    hadoop_conf = spark._jsc.hadoopConfiguration()
+
+    hadoop_conf.set(
+    "fs.s3a.aws.credentials.provider",
+    "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
+    )
+    extract(spark)
 if __name__=="__main__":
     main()
