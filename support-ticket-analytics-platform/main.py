a4c292ce406f19af255072d9faae1adadcb908f1
KaranNegi08 <karannegi.agra@gmail.com>
2026-06-30 23:36:05 +0530

    added main file

--- support-ticket-analytics-platform/main.py
+++ support-ticket-analytics-platform/main.py
@@ -1,16 +1,16 @@
 from glob import glob
 import shutil
-
+import os
 from utils.logger import logger
 from aws.s3_bucket.create_bucket import create_bucket
 from aws.s3_bucket.create_folder import create_folder
 from aws.s3_bucket.upload_file import upload_file
-from config.config import RAW_PREFIX
+from config.config import RAW_PREFIX,RDS_PASS,RDS_USER
 from pyspark_folder.extraction.extract import extract,create_spark_session
 from pyspark.sql import SparkSession
 from pyspark_folder.transformation.transformation import transform
-
-
+from pyspark_folder.loading.load import load
+from sql.cleanup_tables import cleanup_table
 
 def main():
     
@@ -24,7 +24,7 @@ def main():
     # upload_file("datasets/issues.csv", RAW_PREFIX + "issues.csv")
     # upload_file("datasets/issues_change_history.csv",RAW_PREFIX + "issues_change_history.csv")
 
-    logger.info("Pipeline Started....")
+    # logger.info("Pipeline Started....")
     # Step 1 — Create Spark session
     spark = create_spark_session()
     hadoop_conf = spark._jsc.hadoopConfiguration()
@@ -40,7 +40,18 @@ def main():
     # Step 3 — Transform
     logger.info("Starting transformation...")
     fact_df, agent_agg_df, resolution_perf_df, cat_trends_df = transform(issues_df, change_df)
+    fact_df.count()
+    
+    # Step 3 — Load
+    logger.info("Starting load...")
+    load(fact_df, agent_agg_df, resolution_perf_df, cat_trends_df)
+
     spark.stop()
+    logger.info("Pipeline complete...")
+
+
+    
+
 
 
    
