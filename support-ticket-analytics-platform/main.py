54939dd0ebc837d265ecced5bb378a52fce4eb72
KaranNegi08 <karannegi.agra@gmail.com>
2026-07-01 10:30:52 +0530

    added exception handling in main file

--- support-ticket-analytics-platform/main.py
+++ support-ticket-analytics-platform/main.py
@@ -1,59 +1,62 @@
-from glob import glob
-import shutil
-import os
+
 from utils.logger import logger
 from aws.s3_bucket.create_bucket import create_bucket
 from aws.s3_bucket.create_folder import create_folder
 from aws.s3_bucket.upload_file import upload_file
-from config.config import RAW_PREFIX,RDS_PASS,RDS_USER
 from pyspark_folder.extraction.extract import extract,create_spark_session
 from pyspark.sql import SparkSession
 from pyspark_folder.transformation.transformation import transform
 from pyspark_folder.loading.load import load
-from sql.cleanup_tables import cleanup_table
 
 def main():
+    try:
+        #CREATING A S3 BUCKET USING BOTO3 SCRIPTS
+        # create_bucket()
+
+        #CREATING FOLDERS INSIDE S3 BUCKET (raw/ , curated/ )
+        # create_folder()
+
+        #UPLOADING FILES INSIDE S3 BUCKET FOLDERS
+        # upload_file("datasets/issues.csv", RAW_PREFIX + "issues.csv")
+        # upload_file("datasets/issues_change_history.csv",RAW_PREFIX + "issues_change_history.csv")
+
+        # logger.info("Pipeline Started....")
+        # Step 1 — Create Spark session
+        spark = create_spark_session()
+        hadoop_conf = spark._jsc.hadoopConfiguration()
+
+        hadoop_conf.set(
+        "fs.s3a.aws.credentials.provider",
+        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
+        )
+        # Step 2 — Extract
+        logger.info("Starting extraction...")
+        issues_df, change_df =extract(spark)
+
+        # Step 3 — Transform
+        logger.info("Starting transformation...")
+        fact_df, agent_agg_df, resolution_perf_df, cat_trends_df = transform(issues_df, change_df)
+        fact_df.count()
+        
+        # Step 3 — Load
+        logger.info("Starting load...")
+        load(fact_df, agent_agg_df, resolution_perf_df, cat_trends_df)
+
+        spark.stop()
+        logger.info("Pipeline complete...")
     
-    #CREATING A S3 BUCKET USING BOTO3 SCRIPTS
-    # create_bucket()
+    except Exception as e:
+        logger.exception("ETL Pipeline Failed")
+        logger.error(f"Reason : {str(e)}")
 
-    #CREATING FOLDERS INSIDE S3 BUCKET (raw/ , curated/ )
-    # create_folder()
+    finally:
+        if spark is not None:
+            logger.info("Stopping Spark Session...")
+            spark.stop()
+            logger.info("Spark Session stopped.")
 
-    #UPLOADING FILES INSIDE S3 BUCKET FOLDERS
-    # upload_file("datasets/issues.csv", RAW_PREFIX + "issues.csv")
-    # upload_file("datasets/issues_change_history.csv",RAW_PREFIX + "issues_change_history.csv")
-
-    # logger.info("Pipeline Started....")
-    # Step 1 — Create Spark session
-    spark = create_spark_session()
-    hadoop_conf = spark._jsc.hadoopConfiguration()
-
-    hadoop_conf.set(
-    "fs.s3a.aws.credentials.provider",
-    "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
-    )
-    # Step 2 — Extract
-    logger.info("Starting extraction...")
-    issues_df, change_df =extract(spark)
-
-    # Step 3 — Transform
-    logger.info("Starting transformation...")
-    fact_df, agent_agg_df, resolution_perf_df, cat_trends_df = transform(issues_df, change_df)
-    fact_df.count()
-    
-    # Step 3 — Load
-    logger.info("Starting load...")
-    load(fact_df, agent_agg_df, resolution_perf_df, cat_trends_df)
-
-    spark.stop()
-    logger.info("Pipeline complete...")
-
-
+        logger.info("Pipeline execution finished.")
     
 
-
-
-   
 if __name__=="__main__":
     main()
