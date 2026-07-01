8144bf0bae21e6bf87c13a243babdc728a09edc8
KaranNegi08 <karannegi.agra@gmail.com>
2026-07-01 10:41:47 +0530

    added exception handling in cleanup tables

--- support-ticket-analytics-platform/sql/cleanup_tables.py
+++ support-ticket-analytics-platform/sql/cleanup_tables.py
@@ -3,22 +3,28 @@ from config.config import RDS_USER,RDS_DB,RDS_HOST,RDS_PASS
 
 
 def cleanup_table():
-    conn = psycopg2.connect(
-        host=RDS_HOST,
-        database=RDS_DB,
-        user=RDS_USER,
-        password=RDS_PASS
-    )
+    try:
+        conn = psycopg2.connect(
+            host=RDS_HOST,
+            database=RDS_DB,
+            user=RDS_USER,
+            password=RDS_PASS
+        )
 
-    cur = conn.cursor()
+        cur = conn.cursor()
 
-    cur.execute("TRUNCATE TABLE fact_tickets CASCADE;")
-    cur.execute("TRUNCATE TABLE dim_customers CASCADE;")
-    cur.execute("TRUNCATE TABLE dim_agents CASCADE;")
-    cur.execute("TRUNCATE TABLE dim_projects CASCADE;")
-    cur.execute("TRUNCATE TABLE analytics.agent_performance")
-    cur.execute("TRUNCATE TABLE analytics.resolution_performance")
-    cur.execute("TRUNCATE TABLE analytics.category_trends")
+        cur.execute("TRUNCATE TABLE fact_tickets CASCADE;")
+        cur.execute("TRUNCATE TABLE dim_customers CASCADE;")
+        cur.execute("TRUNCATE TABLE dim_agents CASCADE;")
+        cur.execute("TRUNCATE TABLE dim_projects CASCADE;")
+        cur.execute("TRUNCATE TABLE analytics.agent_performance")
+        cur.execute("TRUNCATE TABLE analytics.resolution_performance")
+        cur.execute("TRUNCATE TABLE analytics.category_trends")
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        cur.close()
+        conn.close()
